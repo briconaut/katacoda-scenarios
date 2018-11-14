c81214217f62e9dc4eabb647f813c6fdd1d6c767
Thomas Brunko <tsbo@gft.com>
2018-11-14 14:56:25 +0000

    ...

--- git/010.git-init.verify.sh
+++ git/010.git-init.verify.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env bash
-
-set -e
-
-if [[ ! -d ~/demo ]] ; then exit 1 ; fi
-if [[ ! -d ~/demo/.git ]] ; then exit 1 ; fi
-if [[ ! -f ~/demo/.git/HEAD ]] ; then exit 1 ; fi
-
-diff -q ~/demo/.git/HEAD - <<<"ref: refs/heads/master"
-echo "done"
+[ -d ~/demo ] && [ -d ~/demo/.git ] && [ -f ~/demo/.git/HEAD ] && echo "done"
